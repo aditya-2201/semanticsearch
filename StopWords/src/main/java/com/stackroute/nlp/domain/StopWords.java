45f2dfd87b313f11cc3ed85ed80dff5da99dbdbc
aditya.garg <aditya.garg@cgi.com>
2017-11-28 18:01:22 +0530

    for aws

--- StopWords/src/main/java/com/stackroute/nlp/domain/StopWords.java
+++ StopWords/src/main/java/com/stackroute/nlp/domain/StopWords.java
@@ -14,7 +14,7 @@ public class StopWords {
 			"have", "haven't", "having", "he", "he'd", "he'll", "he's", "her", "here", "here's", "hers", "herself",
 			"him", "himself", "his", "how's", "i", "i'd", "i'll", "i'm", "i've", "if", "in", "into",
 			"isn't", "it", "it's", "its", "itself", "let's", "me", "more", "most", "mustn't", "my", "myself", "no",
-			"nor", "not", "of", "off", "on", "once", "only", "or", "other", "ought", "our", "ours", "ourselves", "out",
+			"nor", "not", "off", "on", "once", "only", "or", "other", "ought", "our", "ours", "ourselves", "out",
 			"over", "own", "same", "shan't", "she", "she'd", "she'll", "she's", "should", "shouldn't", "so", "some",
 			"such", "than", "that", "that's", "the", "their", "theirs", "them", "themselves", "then", "there",
 			"there's", "these", "they", "they'd", "they'll", "they're", "they've", "this", "those", "through", "to",
